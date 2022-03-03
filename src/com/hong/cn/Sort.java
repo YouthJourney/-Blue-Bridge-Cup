3113275fcdaa5aa5d461e9c19b45ae9823b99796
YouthJourney <2205365116@qq.com>
2022-03-03 15:56:33 +0800

    1

--- src/com/hong/cn/Sort.java
+++ src/com/hong/cn/Sort.java
@@ -3,8 +3,8 @@ package com.hong.cn;
 public class Sort {
     static void sort(int[] x) {
         int p = 0;
-        int left = 0; // 最左边的下标
-        int right = x.length - 1; // 最右边的下标
+        int left = 0; // 最左边的下标，左边都小于0
+        int right = x.length - 1; // 最右边的下标，右边都大于0
 
         while(p <= right){
             if(x[p] < 0){
@@ -19,8 +19,16 @@ public class Sort {
                 x[p] = t;
                 right --;
             }else {
-
+                p++;
             }
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {25, 18, -2, 0, 16, -5, 33, 21, 0, 19, -16, 25, -3, 0};
+        sort(arr);
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
         }
     }
 }
